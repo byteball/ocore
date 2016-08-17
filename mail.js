fa118600dfe56b2e327006fd68104ac3e3496a82
tony <tonyofbyteball@users.noreply.github.com>
2016-08-17 21:17:03 +0300

    full path to sendmail, return-path header

--- mail.js
+++ mail.js
@@ -3,20 +3,20 @@ var child_process = require('child_process');
 var conf = require('./conf.js');
 
 function sendmail(params){
-    var child = child_process.spawn('sendmail', ['-t', params.to]);
-    child.stdout.pipe(process.stdout);
-    child.stderr.pipe(process.stderr);
-    child.stdin.write("To: "+params.to+"\r\nFrom: "+params.from+"\r\nSubject: "+params.subject+"\r\n\r\n"+params.body);
-    child.stdin.end();
+	var child = child_process.spawn('/usr/sbin/sendmail', ['-t', params.to]);
+	child.stdout.pipe(process.stdout);
+	child.stderr.pipe(process.stderr);
+	child.stdin.write("Return-Path: <"+params.from+">\r\nTo: "+params.to+"\r\nFrom: "+params.from+"\r\nSubject: "+params.subject+"\r\n\r\n"+params.body);
+	child.stdin.end();
 }
 
 function sendBugEmail(error_message, exception){
-    sendmail({
-        to: conf.bug_sink_email,
-        from: conf.bugs_from_email,
-        subject: 'BUG '+error_message.replace(/\s/g, ' '),
-        body: error_message + "\n\n" + JSON.stringify(exception, null, '\t')
-    });
+	sendmail({
+		to: conf.bug_sink_email,
+		from: conf.bugs_from_email,
+		subject: 'BUG '+error_message.replace(/\s/g, ' '),
+		body: error_message + "\n\n" + JSON.stringify(exception, null, '\t')
+	});
 }
 
 exports.sendmail = sendmail;
