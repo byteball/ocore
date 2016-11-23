e00ac0163f7ceb3bddd8b0df93e39bf5b6240e71
tony <tonyofbyteball@users.noreply.github.com>
2016-11-23 19:51:31 +0300

    left join to force correct index

--- paid_witnessing.js
+++ paid_witnessing.js
@@ -189,9 +189,10 @@ function buildPaidWitnesses(conn, objUnitProps, arrWitnesses, onDone){
 			//throw "no witnesses before mc "+to_main_chain_index+" for unit "+objUnitProps.unit;
 		profiler.start();
 		conn.query( // we don't care if the unit is majority witnessed by the unit-designated witnesses
+			// _left_ join forces use of indexes in units
 			"SELECT address, MIN(main_chain_index-?) AS delay \n\
-			FROM unit_authors \n\
-			JOIN units USING(unit) \n\
+			FROM units \n\
+			LEFT JOIN unit_authors USING(unit) \n\
 			WHERE unit IN(?) AND address IN(?) AND sequence='good' \n\
 			GROUP BY address",
 			[objUnitProps.main_chain_index, arrUnits, arrWitnesses],
