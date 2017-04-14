293569ec78953ac15824e2d3e56cfc20c9103528
kakysha <kakysha@me.com>
2017-04-14 19:42:03 +0300

    message parsing in byteball, removed from core

--- chat_storage.js
+++ chat_storage.js
@@ -10,9 +10,6 @@ function store(correspondent_address, message, is_incoming, type) {
 function load(correspondent_address, up_to_id, limit, cb) {
 	db.query("SELECT id, message, creation_date, is_incoming, type FROM chat_messages \n\
 		WHERE correspondent_address=? AND id < "+up_to_id+" ORDER BY id DESC LIMIT ?", [correspondent_address, limit], function(rows){
-			for (var i in rows) {
-				rows[i] = parseMessage(rows[i]);
-			}
 			cb(rows);			
 		});
 }
@@ -22,17 +19,6 @@ function purge(correspondent_address) {
 		WHERE correspondent_address=?", [correspondent_address]);
 }
 
-function parseMessage(message) {
-	switch (message.type) {
-		case "system":
-			message.message = JSON.parse(message.message);
-			message.message = "chat recording " + (message.message.state ? "&nbsp;" : "") + "<b dropdown-toggle=\"#recording-drop\">" + (message.message.state ? "ON" : "OFF") + "</b><span class=\"padding\"></span>";
-			break;
-	}
-	return message;
-}
-
 exports.store = store;
 exports.load = load;
 exports.purge = purge;
-exports.parseMessage = parseMessage;
