74bde81b7eced1b37f5858d0b2944ab952c14798
MF Nowlan <fitznowlan@gmail.com>
2017-09-11 21:38:27 -0400

    Move getSourceString to String Utils
    This commit is a non-functional change.
    
    It moves the getSourceString function into its own string_utils module.
    Additionally, a new string_utils test is added to check the functionality.
    
    Lastly, it moves the test files into a new sub-directory named test.
    
    **Tested: Locally**
    - Ran all tests in the /test directory and verified they pass.

--- test/validation_utils.test.js
+++ test/validation_utils.test.js
@@ -1,7 +1,7 @@
 const test = require('ava');
 const { check, gen, property } = require('testcheck');
 
-var ValidationUtils = require("./validation_utils.js");
+var ValidationUtils = require("../validation_utils.js");
 
 /**
  * hasFieldsExcept
