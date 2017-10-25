19a7c4a38820a88c7a9668594fd892c40594912b
tony <tonyofbyteball@users.noreply.github.com>
2017-10-25 14:32:35 +0300

    enforce stable in parents since mci 1300000

--- validation.js
+++ validation.js
@@ -471,17 +471,17 @@ function validateParents(conn, objJoint, objValidationState, callback){
 						// if it were not stable, we wouldn't have had the ball at all
 						if (objLastBallUnitProps.ball !== last_ball)
 							return callback("stable: last_ball "+last_ball+" and last_ball_unit "+last_ball_unit+" do not match");
-						if (objValidationState.last_ball_mci <= 800000)
+						if (objValidationState.last_ball_mci <= 1300000)
 							return checkNoSameAddressInDifferentParents();
 					}
 					// Last ball is not stable yet in our view. Check if it is stable in view of the parents
 					main_chain.determineIfStableInLaterUnitsAndUpdateStableMcFlag(conn, last_ball_unit, objUnit.parent_units, objLastBallUnitProps.is_stable, function(bStable){
-						if (!bStable && objLastBallUnitProps.is_stable === 1){
+						/*if (!bStable && objLastBallUnitProps.is_stable === 1){
 							var eventBus = require('./event_bus.js');
 							eventBus.emit('nonfatal_error', "last ball is stable, but not stable in parents, unit "+objUnit.unit, new Error());
 							return checkNoSameAddressInDifferentParents();
 						}
-						else if (!bStable)
+						else */if (!bStable)
 							return callback(objUnit.unit+": last ball unit "+last_ball_unit+" is not stable in view of your parents "+objUnit.parent_units);
 						conn.query("SELECT ball FROM balls WHERE unit=?", [last_ball_unit], function(ball_rows){
 							if (ball_rows.length === 0)
