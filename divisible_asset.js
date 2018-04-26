db62602a46018e16e0d3801bf0144bc69d3439de
tony <tonyofbyteball@users.noreply.github.com>
2018-04-26 19:48:59 +0300

    err msg about fees

--- divisible_asset.js
+++ divisible_asset.js
@@ -365,8 +365,8 @@ function composeMinimalDivisibleAssetPaymentJoint(params){
 		if (arrFundedPayingAddresses.length === 0)
 			return params.callbacks.ifNotEnoughFunds("all paying addresses are unfunded in asset, make sure all your funds are confirmed");
 		composer.readSortedFundedAddresses(null, params.available_fee_paying_addresses, TYPICAL_FEE, function(arrFundedFeePayingAddresses){
-			if (arrFundedPayingAddresses.length === 0)
-				return params.callbacks.ifNotEnoughFunds("all paying addresses are unfunded in asset, make sure all your funds are confirmed");
+			if (arrFundedFeePayingAddresses.length === 0)
+				return params.callbacks.ifNotEnoughFunds("all paying addresses are unfunded in bytes necessary for fees, make sure all your funds are confirmed");
 			var minimal_params = _.clone(params);
 			delete minimal_params.available_paying_addresses;
 			delete minimal_params.available_fee_paying_addresses;
