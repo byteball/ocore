0f8cc7774bf6f20497a06669a836405b1f8a03c4
tony <tonyofbyteball@users.noreply.github.com>
2018-07-16 03:54:08 +0300

    send to usernames

--- uri.js
+++ uri.js
@@ -82,7 +82,7 @@ function parseUri(uri, callbacks){
 		return callbacks.ifError("too many question marks");
 	var address = decodeURIComponent(arrParts[0]);
 	var query_string = arrParts[1];
-	if (!ValidationUtils.isValidAddress(address) && !ValidationUtils.isValidEmail(address))
+	if (!ValidationUtils.isValidAddress(address) && !ValidationUtils.isValidEmail(address) && !address.match(/^(steem\/|reddit\/|@)/i))
 		return callbacks.ifError("address "+address+" is invalid");
 	objRequest.type = "address";
 	objRequest.address = address;
