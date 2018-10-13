0d6b8883fb39b4015897e3e5c59aa85570d08a8e
xjenek <xxjenekxx@gmail.com>
2018-10-13 11:28:01 +0300

    fixes

--- definition.js
+++ definition.js
@@ -597,7 +597,7 @@ function validate_formula(args, complexity, cb) {
 					var value = splitParam[2].trim();
 					dataFeedExists[data_feed] = true;
 					if (variableExists[name]) return false;
-					if (!(/^[a-zA-Z0-9_:\s\-.]+$/.test(value)) || splitParam.length > 3 || value === '') return false;
+					if (!(/^[a-zA-Z0-9_: \-.]+$/.test(value)) || splitParam.length > 3 || value === '') return false;
 					variableExists[name] = true;
 					switch (name) {
 						case 'oracles':
@@ -671,7 +671,7 @@ function validate_formula(args, complexity, cb) {
 			var operator = splitParam[1].trim();
 			var value = splitParam[2].trim();
 			if (variableExists[name]) return false;
-			if (!(/^[a-zA-Z0-9_\s\-.]+$/.test(value)) || splitParam.length > 3 || value === '') return false;
+			if (!(/^[a-zA-Z0-9_ \-.]+$/.test(value)) || splitParam.length > 3 || value === '') return false;
 			switch (name) {
 				case 'address':
 					if (operator !== '=' && operator !== '!=') return false;
