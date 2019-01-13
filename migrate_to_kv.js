b1381a2bd9522783ccd0ae975e02462d86b09aa5
tony <tonyofbyteball@users.noreply.github.com>
2019-01-13 17:16:05 +0300

    migrate data feeds in chunks

--- migrate_to_kv.js
+++ migrate_to_kv.js
@@ -61,57 +61,69 @@ function migrateUnits(conn, onDone){
 }
 
 function migrateDataFeeds(conn, onDone){
-	conn.query(
-		"SELECT unit, address, feed_name, `value`, int_value, main_chain_index \n\
-		FROM data_feeds CROSS JOIN units USING(unit) CROSS JOIN unit_authors USING(unit) \n\
-		ORDER BY data_feeds.rowid",
-		function(rows){
-			if (rows.length === 0)
-				return onDone();
-			var start_time = Date.now();
-			var batch = kvstore.batch();
-			async.forEachOfSeries(
-				rows,
-				function(row, i, cb){
-					var strMci = string_utils.encodeMci(row.main_chain_index);
-					var strValue = null;
-					var numValue = null;
-					var value = null;
-					if (row.value !== null){
-						value = row.value;
-						strValue = row.value;
-						var float = string_utils.getNumericFeedValue(row.value);
-						if (float !== null)
-							numValue = string_utils.encodeDoubleInLexicograpicOrder(float);
-					}
-					else{
-						value = int_value;
-						numValue = string_utils.encodeDoubleInLexicograpicOrder(row.int_value);
-					}
-					// duplicates will be overwritten, that's ok for data feed search
-					if (strValue !== null)
-						batch.put('df\n'+row.address+'\n'+row.feed_name+'\ns\n'+strValue+'\n'+strMci, row.unit);
-					if (numValue !== null)
-						batch.put('df\n'+row.address+'\n'+row.feed_name+'\nn\n'+numValue+'\n'+strMci, row.unit);
-					batch.put('dfv\n'+row.address+'\n'+row.feed_name+'\n'+strMci, value+'\n'+row.unit);
-					
-					if (i%10000 > 0)
-						return (i%1000 === 0) ? setImmediate(cb) : cb();
-					commitBatch(batch, function(){
-						console.error('df '+i);
-						// open a new batch
-						batch = kvstore.batch();
-						cb();
-					});
-				},
-				function(){
-					commitBatch(batch, function(){
-						var consumed_time = Date.now()-start_time;
-						console.error('df done in '+consumed_time+'ms, avg '+(consumed_time/rows.length)+'ms');
-						onDone();
-					});
+	if (conf.storage !== 'sqlite')
+		throw Error('only sqlite migration supported');
+	var count = 0;
+	var offset = 0;
+	var CHUNK_SIZE = 10000;
+	var start_time = Date.now();
+	async.forever(
+		function(next){
+			conn.query(
+				"SELECT unit, address, feed_name, `value`, int_value, main_chain_index \n\
+				FROM data_feeds CROSS JOIN units USING(unit) CROSS JOIN unit_authors USING(unit) \n\
+				WHERE data_feeds.rowid>=? AND data_feeds.rowid<? \n\
+				ORDER BY data_feeds.rowid",
+				[offset, offset + CHUNK_SIZE]
+				function(rows){
+					if (rows.length === 0)
+						return next('done');
+					var batch = kvstore.batch();
+					async.eachSeries(
+						rows,
+						function(row, cb){
+							count++;
+							var strMci = string_utils.encodeMci(row.main_chain_index);
+							var strValue = null;
+							var numValue = null;
+							var value = null;
+							if (row.value !== null){
+								value = row.value;
+								strValue = row.value;
+								var float = string_utils.getNumericFeedValue(row.value);
+								if (float !== null)
+									numValue = string_utils.encodeDoubleInLexicograpicOrder(float);
+							}
+							else{
+								value = int_value;
+								numValue = string_utils.encodeDoubleInLexicograpicOrder(row.int_value);
+							}
+							// duplicates will be overwritten, that's ok for data feed search
+							if (strValue !== null)
+								batch.put('df\n'+row.address+'\n'+row.feed_name+'\ns\n'+strValue+'\n'+strMci, row.unit);
+							if (numValue !== null)
+								batch.put('df\n'+row.address+'\n'+row.feed_name+'\nn\n'+numValue+'\n'+strMci, row.unit);
+							batch.put('dfv\n'+row.address+'\n'+row.feed_name+'\n'+strMci, value+'\n'+row.unit);
+
+							(count % 1000 === 0) ? setImmediate(cb) : cb();
+						},
+						function(){
+							commitBatch(batch, function(){
+								console.error('df '+count);
+								offset += CHUNK_SIZE;
+								next();
+							});
+						}
+					);
 				}
 			);
+		},
+		function(err){
+			if (count === 0)
+				return onDone();
+			var consumed_time = Date.now()-start_time;
+			console.error('df done in '+consumed_time+'ms, avg '+(consumed_time/count)+'ms');
+			onDone();
 		}
 	);
 }
