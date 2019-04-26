e6fd45cdcfee1e6401bb3f0c6595c70433909a38
tony <tonyofbyteball@users.noreply.github.com>
2019-04-26 14:38:48 +0300

    enable any definition change upgrade

--- constants.js
+++ constants.js
@@ -65,5 +65,5 @@ exports.branchedMinMcWlUpgradeMci = exports.bTestnet ? 593000 : 2909000;
 exports.otherAddressInDefinitionUpgradeMci = exports.bTestnet ? 602000 : 2909000;
 exports.attestedInDefinitionUpgradeMci = exports.bTestnet ? 616000 : 2909000;
 exports.altBranchByBestParentUpgradeMci = exports.bTestnet ? 642000 : 3009824;
-exports.anyDefinitionChangeUpgradeMci = exports.bTestnet ? 855000 : Infinity;
+exports.anyDefinitionChangeUpgradeMci = exports.bTestnet ? 855000 : 4229100;
 exports.formulaUpgradeMci = exports.bTestnet ? Infinity : Infinity;
