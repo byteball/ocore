2dca4c6785d967b535fe6f05bd6c80857d5ff8fd
tony <tonyofbyteball@users.noreply.github.com>
2019-06-28 21:03:34 +0300

    test chain of AAs

--- test/aa_composer.test.js
+++ test/aa_composer.test.js
@@ -49,6 +49,19 @@ function addAA(aa) {
 
 var old_cache = {};
 
+// this hack is necessary only for 1-witness network where an AA-response unit can rebuild the MC to itself
+function fixCache() {
+	if (Object.keys(old_cache.assocUnstableUnits).length !== 1)
+		return;
+	for (var unit in storage.assocUnstableUnits) {
+		var objUnit = storage.assocUnstableUnits[unit];
+		if (objUnit.is_free) {
+			objUnit.is_on_main_chain = old_cache.assocUnstableUnits[unit].is_on_main_chain;
+			objUnit.main_chain_index = old_cache.assocUnstableUnits[unit].main_chain_index;
+		}
+	}
+}
+
 test.before.cb(t => {
 	eventBus.once('caches_ready', () => {
 		old_cache.assocUnstableUnits = _.cloneDeep(storage.assocUnstableUnits);
@@ -56,12 +69,6 @@ test.before.cb(t => {
 		old_cache.assocUnstableMessages = _.cloneDeep(storage.assocUnstableMessages);
 		old_cache.assocBestChildren = _.cloneDeep(storage.assocBestChildren);
 		old_cache.assocStableUnitsByMci = _.cloneDeep(storage.assocStableUnitsByMci);
-		// this hack is necessary only for 1-witness network where an AA-response unit can rebuild the MC to itself
-		if (Object.keys(old_cache.assocUnstableUnits).length === 1)
-			for (var unit in old_cache.assocUnstableUnits) {
-				old_cache.assocUnstableUnits[unit].is_on_main_chain = 0;
-				old_cache.assocUnstableUnits[unit].main_chain_index = null;
-			}
 		t.end();
 	});
 });
@@ -95,6 +102,7 @@ test.cb.serial('AA with response vars', t => {
 		t.deepEqual(arrResponses[0].bounced, false);
 		t.deepEqual(arrResponses[0].response.responseVars.received_amount, 40000);
 		t.deepEqual(arrResponses[0].objResponseUnit.messages.find(function (message) { return (message.app === 'payment'); }).payload.outputs.find(function (output) { return (output.address === trigger.address); }).amount, 38000);
+		fixCache();
 		t.deepEqual(storage.assocUnstableUnits, old_cache.assocUnstableUnits);
 		t.deepEqual(storage.assocStableUnits, old_cache.assocStableUnits);
 		t.deepEqual(storage.assocUnstableMessages, old_cache.assocUnstableMessages);
@@ -130,6 +138,89 @@ test.cb.serial('less than bounce fees', t => {
 		t.deepEqual(arrResponses[0].response_unit, null);
 		t.deepEqual(arrResponses[0].objResponseUnit, null);
 		t.deepEqual(arrResponses[0].response.error, "received bytes are not enough to cover bounce fees");
+		fixCache();
+		t.deepEqual(storage.assocUnstableUnits, old_cache.assocUnstableUnits);
+		t.deepEqual(storage.assocStableUnits, old_cache.assocStableUnits);
+		t.deepEqual(storage.assocUnstableMessages, old_cache.assocUnstableMessages);
+		t.deepEqual(storage.assocBestChildren, old_cache.assocBestChildren);
+		t.deepEqual(storage.assocStableUnitsByMci, old_cache.assocStableUnitsByMci);
+		t.end();
+	});
+});
+
+test.cb.serial('chain of AAs', t => {
+	var trigger_address = "I2ADHGP4HL6J37NQAD73J7E5SKFIXJOT";
+	var trigger = { outputs: { base: 40000 }, data: { x: 333 }, address: trigger_address };
+
+	var secondary_aa = ['autonomous agent', {
+		bounce_fees: { base: 10000 },
+		messages: [
+			{
+				app: 'payment',
+				payload: {
+					asset: 'base',
+					outputs: [
+						{address: "{trigger.initial_address}", amount: "{trigger.output[[asset=base]] - 1000}"}
+					]
+				}
+			},
+			{
+				app: 'state',
+				state: `{
+					var['who'] = trigger.address || timestamp;
+					var['initial'] = trigger.initial_address || timestamp;
+				}`
+			}
+		]
+	}];
+	var secondary_address = objectHash.getChash160(secondary_aa);
+	addAA(secondary_aa);
+
+	var primary_aa = ['autonomous agent', {
+		bounce_fees: { base: 10000 },
+		messages: [
+			{
+				app: 'payment',
+				payload: {
+					asset: 'base',
+					outputs: [
+						{address: secondary_address, amount: "{trigger.output[[asset=base]] - 1000}"}
+					]
+				}
+			},
+			{
+				app: 'state',
+				state: `{
+					var['who'] = trigger.address || timestamp;
+					var['initial'] = trigger.initial_address || timestamp;
+				}`
+			}
+		]
+	}];
+	var primary_address = objectHash.getChash160(primary_aa);
+	addAA(primary_aa);
+	
+	aa_composer.dryRunPrimaryAATrigger(trigger, primary_address, primary_aa, (arrResponses) => {
+		t.deepEqual(arrResponses.length, 2);
+		t.deepEqual(arrResponses[0].aa_address, primary_address);
+		t.deepEqual(arrResponses[0].bounced, false);
+		t.deepEqual(arrResponses[0].response.error, undefined);
+		t.deepEqual(arrResponses[0].objResponseUnit.messages.find(function (message) { return (message.app === 'payment'); }).payload.outputs.find(function (output) { return (output.address === secondary_address); }).amount, 39000);
+		t.deepEqual(arrResponses[0].updatedStateVars[primary_address], {
+			who: { value: trigger_address + arrResponses[0].objResponseUnit.timestamp },
+			initial: { value: trigger_address + arrResponses[0].objResponseUnit.timestamp },
+		});
+		t.deepEqual(arrResponses[0].updatedStateVars[secondary_address], {
+			who: { value: primary_address + arrResponses[1].objResponseUnit.timestamp },
+			initial: { value: trigger_address + arrResponses[1].objResponseUnit.timestamp },
+		});
+		
+		t.deepEqual(arrResponses[1].aa_address, secondary_address);
+		t.deepEqual(arrResponses[1].bounced, false);
+		t.deepEqual(arrResponses[1].response.error, undefined);
+		t.deepEqual(arrResponses[1].objResponseUnit.messages.find(function (message) { return (message.app === 'payment'); }).payload.outputs.find(function (output) { return (output.address === trigger_address); }).amount, 38000);
+		t.deepEqual(arrResponses[1].updatedStateVars, undefined);
+		
 		t.deepEqual(storage.assocUnstableUnits, old_cache.assocUnstableUnits);
 		t.deepEqual(storage.assocStableUnits, old_cache.assocStableUnits);
 		t.deepEqual(storage.assocUnstableMessages, old_cache.assocUnstableMessages);
