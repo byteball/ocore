46000719658754c37a539464577461b8a290a981
Papabyte <bencreze@yahoo.fr>
2019-07-15 06:58:43 +0200

    variable name

--- signature.js
+++ signature.js
@@ -25,12 +25,12 @@ exports.verifyMessageWithPemPubKey = function(message, signature, pem_key) {
 	var verify = crypto.createVerify('SHA256');
 	verify.update(message);
 	verify.end();
-	var encodage = ValidationUtils.isValidHexadecimal(signature) ? 'hex' : 'base64';
+	var encoding = ValidationUtils.isValidHexadecimal(signature) ? 'hex' : 'base64';
 	try {
-		return verify.verify(pem_key, signature, encodage)
+		return verify.verify(pem_key, signature, encoding)
 	} catch(e1) {
 		try {
-			return verify.verify({key: pem_key}, signature, encodage) // from Node v11, the key has to be included in an object 
+			return verify.verify({key: pem_key}, signature, encoding) // from Node v11, the key has to be included in an object 
 		} catch(e2) {
 			console.log("exception when verifying with pem key: " + e1 + " " + e2);
 			return false;
