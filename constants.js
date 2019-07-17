fa1d0404ccec32c43aca95c8ea4a03ae24560fb2
tony <tonyofbyteball@users.noreply.github.com>
2019-07-17 12:21:20 +0300

    activate formulas in testnet

--- constants.js
+++ constants.js
@@ -76,7 +76,7 @@ exports.otherAddressInDefinitionUpgradeMci = exports.bTestnet ? 602000 : 2909000
 exports.attestedInDefinitionUpgradeMci = exports.bTestnet ? 616000 : 2909000;
 exports.altBranchByBestParentUpgradeMci = exports.bTestnet ? 642000 : 3009824;
 exports.anyDefinitionChangeUpgradeMci = exports.bTestnet ? 855000 : 4229100;
-exports.formulaUpgradeMci = exports.bTestnet ? Infinity : Infinity;
+exports.formulaUpgradeMci = exports.bTestnet ? 961000 : Infinity;
 exports.witnessedLevelMustNotRetreatFromAllParentsUpgradeMci = exports.bTestnet ? 909000 : Infinity;
 exports.timestampUpgradeMci = exports.bTestnet ? 909000 : Infinity;
 
