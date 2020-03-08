7329cd7534caec7c855a7ca0a58e4de321d93bd4
Tarmo <tarmo888@hotmail.com>
2020-03-08 05:55:21 +0200

    Buffer.from

--- test/login.test.js
+++ test/login.test.js
@@ -12,7 +12,7 @@ var ecdsaSig = require('../signature.js');
 // other implementations can cross reference these tests as long as the key
 // doesn't change
 const challenge = "bUSwwUmABqPGAyRteUPKdaaq/wDM5Rqr+UL3sO/a";
-const priv = new Buffer("18d8bc95d3b4ae8e7dd5aaa77158f72d7ec4e8556a11e69b20a87ee7d6ac70b4", "hex");
+const priv = new Buffer.from("18d8bc95d3b4ae8e7dd5aaa77158f72d7ec4e8556a11e69b20a87ee7d6ac70b4", "hex");
 const pubkey = "AqUMbbXfZg6uw506M9lbiJU/f74X5BhKdovkMPkspfNo"
 
 test.after.always(t => {
