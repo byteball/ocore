a30aeb01f6f396ff0113b83549a01e90b1cb87bd
tony <tonyofbyteball@users.noreply.github.com>
2020-06-10 00:53:27 +0300

    exceptions for some units

--- tools/validate_aa_definitions.js
+++ tools/validate_aa_definitions.js
@@ -1,6 +1,7 @@
 /*jslint node: true */
 'use strict';
 var async = require('async');
+var constants = require('../constants.js');
 var db = require('../db.js');
 var storage = require('../storage.js');
 var aa_validation = require('../aa_validation.js');
@@ -11,10 +12,14 @@ var readGetterProps = function (aa_address, func_name, cb) {
 };
 
 
-db.query("SELECT address, definition, mci FROM aa_addresses ORDER BY rowid", rows => {
+db.query("SELECT address, definition, mci, unit FROM aa_addresses ORDER BY rowid", rows => {
 	async.eachSeries(
 		rows,
 		function (row, cb) {
+			if (constants.bTestnet && ['BD7RTYgniYtyCX0t/a/mmAAZEiK/ZhTvInCMCPG5B1k=', 'EHEkkpiLVTkBHkn8NhzZG/o4IphnrmhRGxp4uQdEkco=', 'bx8VlbNQm2WA2ruIhx04zMrlpQq3EChK6o3k5OXJ130=', '08t8w/xuHcsKlMpPWajzzadmMGv+S4AoeV/QL1F3kBM='].indexOf(row.unit) >= 0) {
+				console.log(row.address, 'skipped');
+				return cb();
+			}
 			var arrDefinition = JSON.parse(row.definition);
 			aa_validation.validateAADefinition(arrDefinition, readGetterProps, row.mci, err => {
 				console.log(row.address, err);
