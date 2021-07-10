031b06e7f0462924110295ccef1516fc5060bcd7
tony <tonyofbyteball@users.noreply.github.com>
2021-07-11 02:16:49 +0300

    faster checkBalances using temp table

--- aa_composer.js
+++ aa_composer.js
@@ -1652,6 +1652,34 @@ function checkBalances() {
 					conn.release();
 					return unlock();
 				}
+				var sql_create_temp = "CREATE TEMPORARY TABLE aa_outputs_balances ( \n\
+					address CHAR(32) NOT NULL, \n\
+					asset CHAR(44) NOT NULL, \n\
+					calculated_balance BIGINT NOT NULL, \n\
+					PRIMARY KEY (address, asset) \n\
+				)";
+				var sql_fill_temp = "INSERT INTO aa_outputs_balances (address, asset, calculated_balance) \n\
+					SELECT address, IFNULL(asset, 'base'), SUM(amount) \n\
+					FROM aa_addresses \n\
+					CROSS JOIN outputs USING(address) \n\
+					CROSS JOIN units ON outputs.unit=units.unit \n\
+					WHERE is_spent=0 AND ( \n\
+						is_stable=1 \n\
+						OR is_stable=0 AND EXISTS (SELECT 1 FROM unit_authors CROSS JOIN aa_addresses USING(address) WHERE unit_authors.unit=outputs.unit) \n\
+					) \n\
+					GROUP BY address, asset";
+				var sql_balances_to_outputs = "SELECT aa_balances.address, aa_balances.asset, balance, calculated_balance \n\
+				FROM aa_balances \n\
+				LEFT JOIN aa_outputs_balances USING(address, asset) \n\
+				GROUP BY aa_balances.address, aa_balances.asset \n\
+				HAVING balance != calculated_balance";
+				var sql_outputs_to_balances = "SELECT aa_outputs_balances.address, aa_outputs_balances.asset, balance, calculated_balance \n\
+				FROM aa_outputs_balances \n\
+				LEFT JOIN aa_balances USING(address, asset) \n\
+				GROUP BY aa_outputs_balances.address, aa_outputs_balances.asset \n\
+				HAVING balance != calculated_balance";
+				var sql_drop_temp = db.dropTemporaryTable("aa_outputs_balances");
+				
 				var stable_or_from_aa = "( \n\
 					(SELECT is_stable FROM units WHERE units.unit=outputs.unit)=1 \n\
 					OR EXISTS (SELECT 1 FROM unit_authors CROSS JOIN aa_addresses USING(address) WHERE unit_authors.unit=outputs.unit) \n\
@@ -1682,9 +1710,12 @@ function checkBalances() {
 					GROUP BY aa_addresses.address, outputs.asset \n\
 					HAVING balance != calculated_balance";
 				async.eachSeries(
-					[sql_base, sql_assets_balances_to_outputs, sql_assets_outputs_to_balances],
+				//	[sql_base, sql_assets_balances_to_outputs, sql_assets_outputs_to_balances],
+					[sql_create_temp, sql_fill_temp, sql_balances_to_outputs, sql_outputs_to_balances, sql_drop_temp],
 					function (sql, cb) {
 						conn.query(sql, function (rows) {
+							if (!Array.isArray(rows))
+								return cb();
 							// ignore discrepancies that result from limited precision of js numbers
 							rows = rows.filter(row => {
 								if (row.balance <= Number.MAX_SAFE_INTEGER || row.calculated_balance <= Number.MAX_SAFE_INTEGER)
