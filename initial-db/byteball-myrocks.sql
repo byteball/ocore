2d104a9181ad219b8f31736feda9ebade9b3b8ae
tony <tonyofbyteball@users.noreply.github.com>
2022-06-08 12:41:13 +0300

    getters in mysql

--- initial-db/byteball-myrocks.sql
+++ initial-db/byteball-myrocks.sql
@@ -764,6 +764,7 @@ CREATE TABLE aa_addresses (
 	storage_size INT NOT NULL DEFAULT 0,
 	base_aa CHAR(32) NULL,
 	definition TEXT NOT NULL,
+	getters TEXT NULL,
 	creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	CONSTRAINT aaAddressesByBaseAA FOREIGN KEY (base_aa) REFERENCES aa_addresses(address)
 ) ENGINE=RocksDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
--- initial-db/byteball-mysql.sql
+++ initial-db/byteball-mysql.sql
@@ -799,6 +799,7 @@ CREATE TABLE aa_addresses (
 	storage_size INT NOT NULL DEFAULT 0,
 	base_aa CHAR(32) NULL,
 	definition TEXT NOT NULL,
+	getters TEXT NULL,
 	creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	CONSTRAINT aaAddressesByBaseAA FOREIGN KEY (base_aa) REFERENCES aa_addresses(address)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
