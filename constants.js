d4311b01eb2cf18268c0ebf833a50df6957e8402
tony <tonyofbyteball@users.noreply.github.com>
2024-11-04 16:00:23 +0700

    0.4.2 min version for full nodes

--- constants.js
+++ constants.js
@@ -74,7 +74,7 @@ exports.TEMP_DATA_PURGE_TIMEOUT = 24 * 3600;
 exports.TEMP_DATA_PRICE = 0.5; // bytes per byte
 
 exports.minCoreVersion = exports.bTestnet ? '0.3.12' : '0.3.12';
-exports.minCoreVersionForFullNodes = exports.bTestnet ? '0.4.0' : '0.4.0';
+exports.minCoreVersionForFullNodes = exports.bTestnet ? '0.4.2' : '0.4.2';
 exports.minCoreVersionToSharePeers = exports.bTestnet ? '0.3.9' : '0.3.9';
 
 exports.lastBallStableInParentsUpgradeMci =  exports.bTestnet ? 0 : 1300000;
